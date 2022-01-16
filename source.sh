6fea8020bdc234f1d18270c737818b5a70f4d86f
chungmcl <chungmcl@cs.washington.edu>
2022-01-15 20:42:14 -0800

    init commit

---
+++ source.sh
@@ -0,0 +1,3 @@
+export RISCV=/home/chungmcl/Projects/keystone/riscv64
+export PATH=/home/chungmcl/Projects/keystone/riscv64/bin:$PATH
+export KEYSTONE_SDK_DIR=/home/chungmcl/Projects/keystone/sdk/build64
